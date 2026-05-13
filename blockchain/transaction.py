8a69aac2b392c95db40df1ebb17e1e6ce2a21e18
Mister-Pepper <alejandro.baeza@cuw.edu>
2026-05-13 01:31:27 -0500

    added to_dict() method so we can pass as an arg in hash method

--- blockchain/transaction.py
+++ blockchain/transaction.py
@@ -7,3 +7,11 @@ class Transaction():
         self.sender_address = sender_address
         self.recip_address = recip_address
         self.amount = amount
+    
+    # need for serialization, will return dict
+    def to_dict(self):
+        return{
+            'sender': self.sender_address,
+            'recipient': self.recip_address,
+            'amount': self.amount
+        }
