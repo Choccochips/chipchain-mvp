923c61a6556c6237cac909ad69fa2fc32e3e30cb
Alejandro Baeza <alejandro.baezaaa@gmail.com>
2026-05-14 00:51:33 -0500

    to_dict was causing issue so i just am returning json version in the fx w/o calling to dict
    in tx.py i ended up changing the name in to_dict since the name discrepencies were causing to and from issues in tx reciepts in front ened

--- blockchain/block.py
+++ blockchain/block.py
@@ -24,7 +24,13 @@ class Block:
 
     def calc_hash(self):
         # transform transactions to pass as arg
-        t_data = [t.to_dict() for t in self.transactions]
+        t_data = []
+        for t in self.transactions:
+            t_data.append({
+                'sender': t.sender_address,
+                'recipient': t.recip_address,
+                'amount': t.amount
+            })
         # need to convert to entire to string as python is giving operand issues
         return_string = str(self.prev_hash) + str(self.timestamp) + str(json.dumps(t_data)) + str(self.nonce)
         return hashlib.sha256(return_string.encode()).hexdigest()
