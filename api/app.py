953a582513ed629243c9ed1bbe6ef43bb3b395be
Alejandro Baeza <alejandro.baezaaa@gmail.com>
2026-05-13 21:26:36 -0500

    built out routes for front end

--- api/app.py
+++ api/app.py
@@ -22,17 +22,44 @@ def index():
 
 @app.route ('/chain')
 def get_chain():
-    chain_data = [block]
+    chain_data = [block.to_dict() for block in chip_chain.chain]
+    return jsonify(chain_data)
 
 @app.route('/balance/<address>')
+def get_balance(address):
+    balance = chip_chain.get_balance(address)
+    # since we get values returned fxs, we can just plug them into json
+    return jsonify({'address': address, 'balance': balance})
 
 @app.route('/mine', methods = ['POST'])
+def mine():
+    data = request.get_json()
+    reward_address = data['address']
+    chip_chain.mine_pending_transactions(reward_address)
+    return jsonify({'message': 'Block has been mined!...'})
 
 @app.route('/transaction', methods = ['POST'])
+# note difference here from 'add', we are creating and know the params we need vs just adding
+# an exisitng transction (from here) to the pending list
+def create_transaction():
+    data = request.get_json()
+    signing_key = SigningKey.from_string(bytes.fromhex(data['private_key']), curve = SECP256k1)
+    sender = signing_key.get_verifying_key().to_string().hex()
+
+    tx = Transaction(sender, data['recipient'], data['amount'])
+    tx.sign_transaction(signing_key)
+    chip_chain.add_transaction(tx)
+    return jsonify({'message': 'Transaction has been added!...'})
 
 @app.route('/validate')
+def validate():
+    is_valid = chip_chain.is_chain_valid()
+    return jsonify({'message': is_valid})
 
 @app.route('/pending')
+def pending():
+    pending = [tx.to_dict() for tx in chip_chain.pending_transactions]
+    return jsonify(pending)
 
 if __name__ == '__main__':
     app.run(debug = True)
