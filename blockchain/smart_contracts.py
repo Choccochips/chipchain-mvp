d32362a7b102b9ceb516b8bfbc6c407c303d508e
Alejandro Baeza <alejandro.baezaaa@gmail.com>
2026-05-21 20:49:00 -0500

    added new file for smart contracts and a simple class for them for now.
    Will beef up later

---
+++ blockchain/smart_contracts.py
@@ -0,0 +1,25 @@
+"""
+
+This file will hold the logic for smart contracts on chipchain. As the file name suggests, smart contracts will be implemented in 
+python for now. Will almost certainly move to an actual existing framework
+
+"""
+
+class SmartContract():
+    def __init__(self, address = None, code = None, state = None, owner = ''):
+        self.address = address
+        # used to dict maps to callable functions 
+        self.code = code or {}
+        # dict for persistant storage
+        self.state = state or {}
+        self.owner = owner
+
+    def execute(self, func_name, args, caller):
+        # check if the function exists in the code
+        if func_name in self.code:
+            # if so we call and pass args
+            to_call = self.code[func_name]
+            return to_call(self.state, args, caller)
+        else:
+            raise Exception('Function does not exist')
+
