ef4f911d1f90e727403b01980d438320fc460c46
Mister-Pepper <alejandro.baeza@cuw.edu>
2026-05-24 01:00:54 -0500

    Ran my first smart contract on chipchain! very very rudimentary, but will develop more adv contracts and test through this file

--- test_contracts.py
+++ test_contracts.py
@@ -32,3 +32,31 @@ contract_code = {
     'mint': mint
 }
 
+# creating a transaction
+tx_1 = Transaction(wallet, None, 0, tx_type='deploy_contract', contract_code=contract_code)
+tx_1.sign_transaction(key)
+
+# now that this is all squared away, we can add it to the chain
+chain1.add_transaction(tx_1)
+# mine the block the tx currently is in (only block we have rn)
+chain1.mine_pending_transactions(wallet)
+contract_address = tx_1.calc_hash()
+
+# since we are calling an exisitng contract, the arguments passed will be a little different
+tx_2 = Transaction(wallet, None, 0, tx_type='call_contract',contract_address=contract_address, function_name='mint', function_args={'amount': 100})
+tx_2.sign_transaction(key)
+chain1.add_transaction(tx_2)
+chain1.mine_pending_transactions(wallet)
+
+# call get balance to see state
+tx_3 = Transaction(wallet, None, 0, tx_type='call_contract', contract_address=contract_address, function_name = 'get_balance', function_args={})
+tx_3.sign_transaction(key)
+chain1.add_transaction(tx_3)
+chain1.mine_pending_transactions(wallet)
+
+contract = chain1.smart_contracts[contract_address]
+result = contract.execute('get_balance', {}, wallet)
+
+# see what shakes
+print(f"Balance: {result}")
+
