6e5cd3c7ef65e57a13b2d4a7ce2c84309364559e
Mister-Pepper <alejandro.baeza@cuw.edu>
2026-05-26 23:57:44 -0500

    added escrow contract functions and began test cases

--- impactivism_contracts.py
+++ impactivism_contracts.py
@@ -17,6 +17,10 @@ chain1 = Blockchain()
 key = SigningKey.generate(curve=SECP256k1)
 wallet = key.get_verifying_key().to_string().hex()
 
+# another key to test 
+key2 = SigningKey.generate(curve=SECP256k1)
+wallet2 = key2.get_verifying_key().to_string().hex()
+
 # --- define functions to be used --- 
 
 # this will add an entry to a caller's list 
@@ -45,32 +49,88 @@ def get_value_add(state, args, caller):
      
     return value_added
 
+# Escrow contract, will have something to do with reputation later
+def create_escrow(state, args, caller):
+    # create a unique ID
+    escrow_id = caller + '-' + args['recipient'] + '-' + str(len(state)) # added counter to have a unique addition to the id
+    entry = {
+        'creator': caller,
+        'recipient': args['recipient'],
+        'value': args['value'],
+        'status': 'pending'
+    }
+
+    # store in state
+    state[escrow_id] = entry
+
+    # return escrow ID so caller has it
+    return escrow_id
+
+def release_escrow(state, args, caller):
+    id = args['escrow_id'] # we look up the id but still need to use it to find escrow entry
+    # some checks
+    if id not in state:
+        raise Exception("This escrow ID does not exist!...")
+    escrow_entry = state[id]
+    if caller != escrow_entry['creator']:
+        raise Exception("Only creator can call this function!...")
+    # change the status if it is still locked
+    if escrow_entry['status'] == 'pending':
+        escrow_entry['status'] = 'released'
+    return escrow_entry['status']
+
+def refund(state, args, caller):
+    # need to get ID
+    id = args['escrow_id']
+    # check that the id is in the list of escrow entries
+    if id not in state:
+        raise Exception("This escrow ID does not exist!...")
+    escrow_entry = state[id]
+
+    # check that the caller is the creator in state
+    if caller != escrow_entry['creator']:
+        raise Exception("Only creator can call this function!...")
+    # update status
+    if escrow_entry['status'] == 'pending':
+        escrow_entry['status'] = 'refunded'
+
+
+"""
+
+Packaging up functions into 'contract_code' for testing
+
+"""
+
 contract_code = {
     # add functions we just defined
-    'log_impact': log_impact,
-    'get_history': get_history,
-    'get_value_add': get_value_add
+    'create_escrow': create_escrow,
+    'release_escrow': release_escrow,
+    'refund': refund
 }
 
-# full test cycle
+
+# This can stay throughout changes as we are jusy deploying the contract 
 tx_1 = Transaction(wallet,None, 0, tx_type = 'deploy_contract', contract_code=contract_code)
 tx_1.sign_transaction(key)
 chain1.add_transaction(tx_1)
 chain1.mine_pending_transactions(wallet) # now that smart contract should be deployed
 
 contract_address = tx_1.calc_hash() # to know which contract to call w/ address
-tx_2 = Transaction(wallet, None, amount = 0, contract_address=contract_address, tx_type= 'call_contract', function_name='log_impact', function_args={'description':'Packaged water bottle', 'value': 50})
+tx_2 = Transaction(wallet, None, 0, contract_address=contract_address, tx_type= 'call_contract', function_name='create_escrow', function_args={'recipient': wallet2, 'value': 500})
 tx_2.sign_transaction(key)
 chain1.add_transaction(tx_2)
 chain1.mine_pending_transactions(wallet)
 
+# we will need to get the escrow_id to then release the funds, we know it gets returned from executing the function. store address
+contract = chain1.smart_contracts[contract_address]
+
 # one more impact
-tx_3 = Transaction(wallet, None, amount = 0, tx_type= 'call_contract', contract_address=contract_address, contract_code=contract_code, function_name='log_impact', function_args={'description': 'sold water', 'value': 25})
+tx_3 = Transaction(wallet, None, 0, contract_address=contract_address, tx_type= 'call_contract', function_name='release_escrow', function_args={'escrow_id': escrow_id})
 tx_3.sign_transaction(key)
 chain1.add_transaction(tx_3)
 chain1.mine_pending_transactions(wallet)
 
-# output
+# output 
 contract = chain1.smart_contracts[contract_address]
 print(f"History: ", contract.execute('get_history', {}, wallet))
 print(f"Total impact: ", contract.execute('get_value_add', {}, wallet))
