d42f5af8a58b2862d20966eb65164a796f660bf1
Mister-Pepper <alejandro.baeza@cuw.edu>
2026-06-09 02:28:28 -0500

    empty for voting power. will fill out skelly later. changing methods in chain.py to be used here.

---
+++ blockchain/contracts/governance.py
@@ -0,0 +1,15 @@
+"""
+
+This file will handle funcs related to voting power. There is no decision making component yet
+Just need to be able to see if we can pick up on wallet power based on tokens
+
+"""
+
+def assign_voting_power(state, args, caller):
+    # check if admin which technically doesnt exist right now
+    if 'admin' not in state:
+        
+    return 
+
+def get_voting_power(state, args, caller):
+    return 
